3a4bdde7103c827ab8a4ca5a3b0cd6b2b703cc58
Vadim Fattakhov <vadfthv@gmail.com>
2023-11-17 22:07:34 -0600

    connectDB and authRouter

--- app.js
+++ app.js
@@ -4,21 +4,26 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// connectDB
+const connectDB = require('./db/connect');
+// routers
+const authRouter = require('./routes/authRouter');
+
 // error handlers
 const notFountMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
+// extra packages
 
-// routers
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+// routes
+app.use('/api/v1/auth', authRouter);
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}`));
   } catch (error) {
     console.log(error);
