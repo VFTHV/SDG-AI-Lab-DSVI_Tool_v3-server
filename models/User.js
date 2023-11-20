a0cc4452a28b26cc4bb960bc1151dbf340dc83bc
Vadim Fattakhov <vadfthv@gmail.com>
2023-11-19 19:25:38 -0600

    Handling password compare in login controller

--- models/User.js
+++ models/User.js
@@ -30,6 +30,11 @@ UserSchema.pre('save', async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
 UserSchema.methods.createJWT = function () {
   return jwt.sign(
     { userId: this._id, name: this.name },
