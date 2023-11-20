e6d3141a36e93701b3f70ce295a60eb2f54b80e0
Vadim Fattakhov <vadfthv@gmail.com>
2023-11-19 20:50:24 -0600

    Finished authentication.js

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -12,7 +12,7 @@ const auth = async (req, res, next) => {
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    // attach the user to the job routes
+    // attach the user to req object, so it could be used in next middleware
     req.user = { userId: payload.userId };
     next();
   } catch (error) {
