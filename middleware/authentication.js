ed9cf9f83da06aca77311303263dcbd01b316148
Vadim Fattakhov <vadfthv@gmail.com>
2023-11-24 21:25:18 -0600

    Added token

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -13,6 +13,7 @@ const auth = async (req, res, next) => {
   // adding a token
   // adding a token
   // adding a token
+  // adding a token
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
