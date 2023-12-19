ab3a960300765099daf2e62974b88086ae8e75f0
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-18 21:46:06 -0600

    Auth controller

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { register, login } = require('../controllers/authController');
 
 // new auth controller
+// new auth controller
 
 router.post('/register', register);
 router.post('/login', login);
