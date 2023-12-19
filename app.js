b6cae82c847318df8e250310ecf5592e845beed0
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-18 21:45:55 -0600

    Auth controller

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 // connectDB
 const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
 // routers
 const authRouter = require('./routes/authRouter');
 
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
 
+// new auth controller
+
 router.post('/register', register);
 router.post('/login', login);
 
