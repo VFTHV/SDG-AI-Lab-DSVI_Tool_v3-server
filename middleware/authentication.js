d8d30fcefe5290f0a623fe00578d9541caff3ff4
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-22 20:49:18 -0600

    Authentication rule

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -9,6 +9,7 @@ const auth = async (req, res, next) => {
     throw new UnauthenticatedError('Authentication invalid');
   }
   const token = authHeader.split(' ')[1];
+  // authentication rule
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
