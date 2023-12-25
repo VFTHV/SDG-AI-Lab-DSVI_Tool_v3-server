848be4029f2c3b0a1f4209a389d09663d8117f51
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-25 17:17:53 -0600

    Not found error

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -2,5 +2,6 @@ const notFound = (req, res) => res.status(404).send('Route does not exist');
 // not found error
 // not found error
 // not found error
+// not found error
 
 module.exports = notFound;
