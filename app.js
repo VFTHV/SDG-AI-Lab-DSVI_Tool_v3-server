4f8cd1800657f8e0a0fbc4fd20b34a7c7f314209
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-26 18:22:06 -0600

    Authenticate user middleware

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const connectDB = require('./db/connect');
 // authenticate user middleware
 // authenticate user middleware
 // authenticate user middleware
+// authenticate user middleware
 
 const authenticateUser = require('./middleware/authentication');
 // routers
