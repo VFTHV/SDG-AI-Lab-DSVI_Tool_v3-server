473dbd0473906b987551ab66e7662160f7e2dcb8
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-29 21:45:25 -0600

    Bad request error

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,11 +14,7 @@ const register = async (req, res) => {
   // if (emailAlreadyExists) {
   //   throw new CustomError.BadRequestError('Email already exists');
   // }
-  // bad request error
-  // bad request error
-  // bad request error
-  // bad request error
-  // bad request error
+
   res.status(StatusCodes.CREATED).json({
     user: { name: user.name, email: user.email, token },
   });
