6eaa6c8393e221902fc41f199fafa88c20a20724
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-29 21:45:16 -0600

    Bad request error

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,6 +18,7 @@ const register = async (req, res) => {
   // bad request error
   // bad request error
   // bad request error
+  // bad request error
   res.status(StatusCodes.CREATED).json({
     user: { name: user.name, email: user.email, token },
   });
