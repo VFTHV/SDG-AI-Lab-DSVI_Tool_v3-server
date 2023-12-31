17de3a5bb7c4fcff121a02d79998b1287a50a8fe
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-31 14:54:12 -0600

    Layers route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,5 +6,6 @@ router.post('/register', register);
 router.post('/login', login);
 // router.post('/layers', login);
 // router.post('/layers', login);
+// router.post('/layers', login);
 
 module.exports = router;
