7b3e4c6ea5dc82cc3c00516305447362a99b58c5
Vadim Fattakhov <vadfthv@gmail.com>
2023-12-31 14:54:39 -0600

    Layers route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,10 +4,5 @@ const { register, login } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
-// router.post('/layers', login);
-// router.post('/layers', login);
-// router.post('/layers', login);
-// router.post('/layers', login);
-// router.post('/layers', login);
 
 module.exports = router;
