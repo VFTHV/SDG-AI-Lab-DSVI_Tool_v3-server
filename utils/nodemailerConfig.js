13a238f2822266c9071828874ae5817db9792c94
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-01 19:33:35 -0600

    Refactor sendEmail with args

---
+++ utils/nodemailerConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'christop.koch52@ethereal.email',
+    pass: 'jKxSs1ZWDTsaFNxkGb',
+  },
+};
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,23 +1,17 @@
 const nodemailer = require('nodemailer');
+const nodemailerConfig = require('./nodemailerConfig');
 
-const sendEmail = async (req, res) => {
+const sendEmail = async ({ to, subject, html }) => {
   let testAccount = await nodemailer.createTestAccount();
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-      user: 'christop.koch52@ethereal.email',
-      pass: 'jKxSs1ZWDTsaFNxkGb',
-    },
-  });
+  const transporter = nodemailer.createTransport(nodemailerConfig);
 
   const info = await transporter.sendMail({
-    from: '"VFTHV" <vadfthv@gmail.com>', // sender address
-    to: 'user@user.com', // list of receivers
-    subject: 'Testing SDG back-end ✔', // Subject line
-    text: 'SDG back end?', // plain text body
-    html: '<b>sdg back end</b>', // html body
+    from: '"VFTHV" <sdgadmin@gmail.com>', // sender address
+    to, // list of receivers
+    subject, // Subject line
+    html,
+    // text: 'SDG back end?', // plain text body
   });
 };
 
