2186b9ca7ba52d178906a9f4b8fa7f97a6e42a14
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-10 19:49:18 -0600

    To delete the createJWT method

--- models/User.js
+++ models/User.js
@@ -47,13 +47,6 @@ UserSchema.methods.comparePassword = async function (candidatePassword) {
 };
 
 // may need to delete the createJWT method
-// may need to delete the createJWT method
-// may need to delete the createJWT method
-// may need to delete the createJWT method
-// may need to delete the createJWT method
-// may need to delete the createJWT method
-// may need to delete the createJWT method
-// may need to delete the createJWT method
 
 UserSchema.methods.createJWT = function () {
   return jwt.sign(
