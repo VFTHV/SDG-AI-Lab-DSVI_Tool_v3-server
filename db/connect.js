c1ae7c85dde59f74ff25f72ae9b2519bb316003d
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-18 20:59:09 -0700

    Connect to DB functionality

--- db/connect.js
+++ db/connect.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 // connect to DB functionality
 // connect to DB functionality
 // connect to DB functionality
+// connect to DB functionality
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
