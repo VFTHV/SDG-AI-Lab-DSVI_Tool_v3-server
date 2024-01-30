4271654a2642680c0d15380da79987ad17a694eb
Vadim Fattakhov <vadfthv@gmail.com>
2024-01-29 21:03:58 -0600

    Update password functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -138,6 +138,7 @@ const logout = async (req, res) => {
 
 const updateUser = async (req, res) => {
   // updating password here
+  // updating password here
 };
 
 module.exports = { register, login, verifyEmail, authenticateRouting, logout };
