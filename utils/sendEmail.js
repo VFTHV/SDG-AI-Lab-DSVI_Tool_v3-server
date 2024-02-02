cb56a0380826f31155fcc41c6a970710cdd25ae0
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-01 21:51:18 -0600

    nodemailer config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,12 +6,6 @@ const sendEmail = async ({ to, subject, html }) => {
   let testAccount = await nodemailer.createTestAccount();
 
   const transporter = nodemailer.createTransport(nodemailerConfig);
-  // nodemailer config
-  // nodemailer config
-  // nodemailer config
-  // nodemailer config
-  // nodemailer config
-  // nodemailer config
 
   const info = await transporter.sendMail({
     from: '"VFTHV" <sdgadmin@gmail.com>', // sender address
