d894cf66e6fda53d64b52037624f5f526a36fb52
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-17 23:08:11 -0600

    createTokenUser

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,12 @@
 const sendVerificationEmail = require('./sendVerificationEmail');
 const { attachCookiesToResponse, isTokenValid } = require('./jwt');
 const { hashPassword } = require('./password');
+const createTokenUser = require('./createTokenUser');
 
 module.exports = {
   sendVerificationEmail,
   attachCookiesToResponse,
   isTokenValid,
   hashPassword,
+  createTokenUser,
 };
