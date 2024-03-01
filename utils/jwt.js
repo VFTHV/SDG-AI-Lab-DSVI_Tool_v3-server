d5c154555ec0db6cb7448f78bb21e44077433d4a
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-29 22:05:04 -0600

    sameSite false

--- utils/jwt.js
+++ utils/jwt.js
@@ -20,7 +20,7 @@ const attachCookiesToResponse = ({ res, user }) => {
     expires: new Date(Date.now() + oneDay * 30),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
-    sameSite: 'Lax',
+    sameSite: false,
   });
 };
 
