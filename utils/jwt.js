d66ee38eb6d6b7fcec50592dce0bf87c90c76170
Vadim Fattakhov <vadfthv@gmail.com>
2024-02-29 22:18:37 -0600

    changed cookies options

--- utils/jwt.js
+++ utils/jwt.js
@@ -16,11 +16,10 @@ const attachCookiesToResponse = ({ res, user }) => {
   const oneDay = 1000 * 60 * 60 * 24;
 
   res.cookie('token', token, {
-    // httpOnly: true,
+    httpOnly: true,
     expires: new Date(Date.now() + oneDay * 30),
-    // secure: process.env.NODE_ENV === 'production',
-    // signed: true,
-    // sameSite: false,
+    secure: process.env.NODE_ENV === 'production',
+    signed: true,
   });
 };
 
