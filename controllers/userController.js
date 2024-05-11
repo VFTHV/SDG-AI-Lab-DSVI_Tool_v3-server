21f3601db9903dcee6ec884fae4dd045d5a4bb68
Vadim Fattakhov <vadfthv@gmail.com>
2024-05-10 20:22:54 -0500

    get users controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,7 @@ const getUsers = async (req, res) => {
   // get users controller
   // get users controller
   // get users controller
+  // get users controller
 
   let searchTerm = null;
   if (email) {
