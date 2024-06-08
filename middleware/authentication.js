9ac46128366675e97be9d7a616f3dbd109a14a7c
Vadim Fattakhov <vadfthv@gmail.com>
2024-06-07 21:50:52 -0500

    refreshToken item

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -29,6 +29,7 @@ const authenticateUser = async (req, res, next) => {
   // refreshToken item
   // refreshToken item
   // refreshToken item
+  // refreshToken item
 
   try {
     const payload = isTokenValid(refreshToken);
